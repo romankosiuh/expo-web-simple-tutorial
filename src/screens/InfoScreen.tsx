4518ede6761cf21af030f89c475a081adc481360
Roman Kosiuh <romankosiuh@gmail.com>
2021-07-02 21:09:01 +0200

    Fixex punctuation

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -10,8 +10,8 @@ const InfoScreen = () => (
             <Text category="p1">
                 Explain to the duck what your code is supposed to do,
                 and then go into detail and explain your code line by line.
-                At some point you will tell the duck what you are doing next and
-                then realise that that is not in fact what you are actually doing.
+                At some point, you will tell the duck what you are doing next and
+                then realize that that is not in fact what you are actually doing.
                 The duck will sit there serenely, happy in the knowledge that it has helped
                 you on your way.
             </Text>
